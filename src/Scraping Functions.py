3fda0b4f4ee2eb427d3fff84867c1de310507eab
TarunApp <tappannagari@gmail.com>
2018-12-01 21:29:14 -0600

    Moved Functions to sperate file, next on list - Create text are for the application

--- src/Scraping Functions.py
+++ src/Scraping Functions.py
@@ -5,23 +5,10 @@ import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 
-#------------SCRAPING------------------#
-#url = ""
 
-#request = requests.get(url)		#<---------- Move this into a seperate function
-#html_content = request.text
+#Functions Should be Easier to Handle with seperate file systems
+#Variables should be changed
 
-#soup = BeautifulSoup(html_content, "html.parser")
-
-#Find the tag and its attribute
-#print(soup.findAll("div", class_ = "paragraph"))
-
-
-#Use this model to find a particular element, this example is not restricted to links
-#links = (soup.find_all("a"))
-#for link in links:
-    #print(link.get("href"))
-#------------------------Functions---------------------------#
 def scrape():
     url = x.get()                #<--------- Use Get function to get text from text boxes
     request = requests.get(url)		#Moved into seperate Function
@@ -55,21 +42,3 @@ def scrapetext():
     #Find the tag and its attribute
     y = soup.findAll("a", text="Exampel") #Find certain text, but with certain tags
     return y
-
-
-
-
-
-#---------------------------GUI----------------------------#
-root = tk.Tk()
-
-Label(root, text="Hello World").grid(row=0, column=0)
-x = Entry(root) #<----- Split the formatting and the initialized variable, otherwise the code doesnt work
-x.grid(row = 0, column = 2)          #<-- Can use Pack, but .grid allows for more flexibility
-Button(root, text="Enter", command=scrape).grid(row=1)  #<---- The third paramater, command, doesnt need any parentheses
-
-
-
-
-root.mainloop()
-
