3d78eeb01fab96ebbbdf73fb403ee760627edfc9
TarunApp <tappannagari@gmail.com>
2019-01-01 15:48:50 -0600

    Updated functions

--- src/Integration/googleapiintegration.py
+++ src/Integration/googleapiintegration.py
@@ -54,47 +54,16 @@ def namecollect(websiteurl):
 	r = h[2]
 	#print(r)
 	
-	#if r in names:
-		#print(True)
-		#print(r) 
-	#else:
-		#print(False)
-		#return 0
+	def findtext(websiteurl):
+	para = soup.body.findAll(text=re.compile(''))
+	para2 = soup.body.findAll(text=re.compile(''))
+	paracombine = para + para2
+	print(paracombine)
+	if "" or "" in paracombine:
+		return True
+	else:
+		return False
 
-	a = len(r)
-	random.seed(time)
-	rand1 = random.randrange(0, a)
-	rand2 = random.randrange(0, a)
-	rand3 = random.randrange(0, a)
-	rand4 = random.randrange(0, a)
-	#rand5 = random.randrange(0, a)
-	#rand6 = random.randrange(0, a)
-
-	list(r)
-	r1 = r[rand1]
-	r2 = r[rand2]
-	r3 = r[rand3]
-	r4 = r[rand4]
-	#r5 = r[rand5]
-	#r6 = r[rand6]
-	print(r)
-	print(r1, r2, r3, r4)
-	rlist = []
-	rlist.append(r1)
-	rlist.append(r2)
-	rlist.append(r3)
-	rlist.append(r4)
-
-	#asdjke = ""
-	#str(asdjke)
-
-	#if r1 and r2 and r3 and r4 in names:
-		#print(True)
-	#else:
-		#print(False)
-
-	for i in names:
-		print(i.lstrip(r1) and i.lstrip(r2) and i.lstrip(r3) and i.lstrip(r4)) 
 #-----------------------------Google Sheets Display--------------------------------------#
 
 
