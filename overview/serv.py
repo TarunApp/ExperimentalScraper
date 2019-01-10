e1f3d83f32126f1f168a4de61e26ff4b4d61996c
TarunApp <tappannagari@gmail.com>
2019-01-09 23:54:18 -0600

    Added scraping function to serv

--- overview/serv.py
+++ overview/serv.py
@@ -3,16 +3,43 @@ import matplotlib.pyplot as plt
 import time, random
 
 
-#x = np.linspace(0, 2, 1)
+
+
+
 							#Function to distribute data points throughout graph
-x = random.seed(time)
-y = random.randint(0, 19029523)
-np.random.seed(y)
+
+url = "https://jasonduongism.weebly.com/"
+request = requests.get(url)  # <---------- Move this into a seperate function
+html_content = request.text
+soup = BeautifulSoup(html_content, "html.parser")
+
+def checktext():
+    para = soup.body.findAll(text=re.compile('The purpose'))
+    para2 = soup.body.findAll(text=re.compile('Click on the tabs'))
+    paracombine = para + para2
+    list(para)
+    print(para)
+    if "The purpose" in para or paracombine:
+        print("True1")
+        return True
+        if "Click on" in para or para2:
+            print("True2")
+            return True
+        else:
+            print("False")
+            return False
+    else:
+        print("False else")
+        return False
 
 
-N = 50
-x = np.random.rand(N) * 2
-y = np.random.rand(N) * 5
-fig, plot1 = plt.subplots()
-plot1.scatter(x, y, 'bo')
-#plt.show()
+
+#Add points to x, y 
+x = []
+y = []
+
+
+#fig, plot1 = plt.subplots()
+plot1 = pl
+plt.scatter(x, y, 100, 'b')
+plt.show()
